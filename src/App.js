3684b321a409da11076c83374555716547321e32
Saurya Dhwoj Acharya <saurya.dacharya@gmail.com>
2020-02-09 12:49:54 +1100

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I am changed</p>
+        <p>The new change is deployed</p>
         <a
           className="App-link"
           href="https://reactjs.org"
